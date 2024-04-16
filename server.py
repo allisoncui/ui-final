b2f693c4ebaaea615c38fffb07b5bbe325a4e293
alyucu6 <allisonycui@gmail.com>
2024-04-16 14:52:14 -0400

    template files

---
+++ server.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from flask import render_template
+from flask import Response, request, jsonify, redirect, url_for
+app = Flask(__name__)
+
+# ROUTES
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
