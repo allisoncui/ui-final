8847f188259bcfd5a664475806ffa6358d797b7c
Kevin Ma <kevinma0522@gmail.com>
2024-04-16 20:00:42 -0400

    intro.html implemented

--- server.py
+++ server.py
@@ -8,6 +8,17 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('home.html')
+@app.route('/intro')
+def intro():
+    return render_template('intro.html')
+@app.route('/strategies')
+def strategies():
+    # Logic to render strategies page
+    return render_template('strategies.html')
 
+@app.route('/quiz')
+def quiz():
+    # Logic to render quiz page
+    return render_template('quiz.html')
 if __name__ == '__main__':
     app.run(debug=True)
